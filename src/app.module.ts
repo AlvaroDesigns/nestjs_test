2aa4f96cf844d1596e8b46084e4cf60320dc9bba
Alvaro saiz <hello@alvarodesigns.com>
2024-11-21 00:53:16 +0100

    Feat: add logic prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { Module } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module";
+import { ItineraryModule } from "./itinerary/itinerary.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule, ItineraryModule, PrismaModule],
 })
 export class AppModule {}
